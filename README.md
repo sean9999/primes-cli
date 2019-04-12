e70ade26742173941348ccd59bcdc833f643e24e
Sean Macdonald <sean@crazyhorsecoding.com>
2019-04-12 19:25:51 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#	Nearest Prime
+
+In order to avoid thundering herd problems, and just for fun, I like to use prime numbers in config files and cron jobs.
+
+This small utility takes a number returns the nearest primes.
+
+If given a prime, it returns 3 numbers: The nearest lesser prime, itself, and the nearest greater prime
+
+If given a non-prime, it returns 2 numbers: nearest lesser prime, and nearest greater.
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use primes::PrimeSet;
+
+//	accept a number on stdin and return the nearest primes
+
+fn main() {
+
+	let mut i = String::new();
+	io::stdin().read_line(&mut i).expect("stdin is not available");
+
+	let i: u64 = match i.trim().parse() {
+		Ok(num) => num,
+		Err(e) => {
+			//	complain and die
+			eprintln!("Not a number. {}", e);
+			std::process::exit(1);
+		}
+	};
+
+	//	find primes
+	let mut pset = PrimeSet::new();
+	let (p2i, p2) = pset.find(i);
+	let p1 = pset.get(p2i-1);
+	if p2 == i {
+		let p3 = pset.get(p2i+1);
+		println!("{},{},{}",p1,p2,p3);
+	} else {
+		println!("{},{}",p1,p2);
+	}
+
+	//	exit normally
+	std::process::exit(0);
+
+}
